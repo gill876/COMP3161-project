067c3c7747058b8c0abce4d3a22e942656eaaca9
Cargill Seiveright <microcargill@hotmail.com>
2020-05-02 03:37:10 -0500

    update name generator, add trigger to user table

--- database/mybook.sql
+++ database/mybook.sql
@@ -10,19 +10,31 @@ DROP TABLE IF EXISTS friends;
 DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS csv_users;
 /*SHOW WARNINGS;*/
+drop trigger IF EXISTS Load_User; /*add trigger*/
+SHOW WARNINGS;
+
+CREATE TABLE csv_users(
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(70) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    firstname VARCHAR(75) NOT NULL,
+    lastname VARCHAR(75) NOT NULL,
+    gender VARCHAR(10) NOT NULL
+);
 
 CREATE TABLE user(
-    user_id INT NOT NULL,
+    user_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(100) NOT NULL UNIQUE,
     email_address VARCHAR(70) NOT NULL,
-    password VARCHAR(256) NOT NULL,
+    user_password VARCHAR(256) NOT NULL,
     datejoined DATE NOT NULL,
     PRIMARY KEY(user_id)
 );
 
 CREATE TABLE profile(
-    profile_id INT NOT NULL,
+    profile_id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     firstname VARCHAR(75) NOT NULL,
     lastname VARCHAR(75) NOT NULL,
@@ -37,27 +49,27 @@ CREATE TABLE profile(
 
 CREATE TABLE post(
     post_id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
+    /*user_id INT NOT NULL,*/
     content VARCHAR(300) NOT NULL, /*change in data dictionary*/
-    timestamp DATETIME NOT NULL,
-    location VARCHAR(70) NOT NULL,
-    PRIMARY KEY(post_id),
-    FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+    time_stamp DATETIME NOT NULL,
+    post_location VARCHAR(70) NOT NULL,
+    PRIMARY KEY(post_id)/*,
+    FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE*/
 );
 
 CREATE TABLE user_group( /*change in data dictionary*/
     group_id INT NOT NULL AUTO_INCREMENT, /*change in data dictionary*/
     group_name VARCHAR(90) NOT NULL,
-    description VARCHAR(300) NOT NULL, /*change in data dictionary*/
+    group_description VARCHAR(300) NOT NULL, /*change in data dictionary*/
     PRIMARY KEY(group_id)
 );
 
 CREATE TABLE comment(
     comment_id INT NOT NULL AUTO_INCREMENT, /*change in data dictionary*/
     post_id INT NOT NULL,
-    text VARCHAR(300),
-    timestamp DATETIME NOT NULL,
-    location VARCHAR(70) NOT NULL,
+    comment VARCHAR(300),
+    time_stamp DATETIME NOT NULL,
+    c_location VARCHAR(70) NOT NULL,
     PRIMARY KEY(comment_id, post_id),
     FOREIGN KEY(post_id) REFERENCES post(post_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -65,7 +77,7 @@ CREATE TABLE comment(
 CREATE TABLE friends(
     friend_id INT NOT NULL AUTO_INCREMENT, /*change in data dictionary*/
     user_id INT NOT NULL,
-    type ENUM('WORK', 'SCHOOL', 'RELATIVE', 'OTHER') NOT NULL,/*change in data dictionary*/
+    friend_type ENUM('WORK', 'SCHOOL', 'RELATIVE', 'OTHER') NOT NULL,/*change in data dictionary*/
     PRIMARY KEY(friend_id),
     FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -75,7 +87,7 @@ CREATE TABLE images(
     post_id INT NOT NULL,
     caption VARCHAR(30),
     file_name VARCHAR(256) NOT NULL, /*change in data dictionary*/
-    timestamp DATETIME NOT NULL, /*change in data dictionary*/
+    time_stamp DATETIME NOT NULL, /*change in data dictionary*/
     PRIMARY KEY(image_id),
     FOREIGN KEY(post_id) REFERENCES post(post_id) ON DELETE CASCADE ON UPDATE CASCADE 
 );
@@ -110,3 +122,17 @@ CREATE TABLE contains(
     FOREIGN KEY(post_id) REFERENCES post(post_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY(image_id) REFERENCES images(image_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+DELIMITER $$
+    CREATE TRIGGER Load_User
+    AFTER INSERT ON csv_users
+    FOR EACH ROW
+    BEGIN
+    INSERT INTO user(username, email_address, user_password, datejoined)
+    /*SELECT * FROM 
+    (SELECT (username, email, SHA2(password, 256), CURDATE())
+    FROM csv_users) AS T;*/
+    VALUES
+    (NEW.username, NEW.email, SHA2(NEW.password, 256), CURDATE());
+    END $$
+DELIMITER ;
